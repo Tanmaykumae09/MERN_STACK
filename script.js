0b914dc5b7cb0bb68e9184c43942c1501a2f7280
TanmayKumar09 <tanmaykumar9274@email.com>
2026-03-10 18:05:51 +0530

    call backs

--- script.js
+++ script.js
@@ -158,18 +158,3 @@ setTimeout(() => {
 console.log("The end")
 //call backs 
 
-const fn = () => {
-    console.log("hi")
-}
-const callback = (arg, fn) => {
-    console.log(arg)
-    fn();
-}
-const loadScript = (src, callback) => {
-    let sc = document.createElement("script")
-    sc.src = src;
-    sc.onlaod = callback("tanmay",fn)
-    document.head.append(sc)
-}
-
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback);
